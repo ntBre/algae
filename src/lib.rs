ca9dff040d3b00bee9536c2f1357df308040bf8d
Brent Westbrook <brentrwestbrook@gmail.com>
2022-11-25 13:04:37 -0500

    move config to its own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,6 @@
 #![feature(iter_collect_into, let_chains)]
 
-pub mod config {
-    use std::time::{self, Duration, SystemTime, UNIX_EPOCH};
-
-    #[allow(unused)]
-    pub struct Config {
-        prompt: String,
-        format: String,
-        rat_format: String,
-        format_verb: String,
-        format_prec: usize,
-        format_float: bool,
-        origin: usize,
-        seed: u64,
-        max_bits: usize,
-        max_digits: usize,
-        max_stack: usize,
-        float_prec: usize,
-        real_time: time::Duration,
-        user_time: time::Duration,
-        sys_time: time::Duration,
-        input_base: usize,
-        output_base: usize,
-    }
-
-    impl Config {
-        pub fn input_base(&self) -> usize {
-            self.input_base
-        }
-    }
-
-    impl Default for Config {
-        fn default() -> Self {
-            Self {
-                prompt: String::new(),
-                format: String::new(),
-                rat_format: String::new(),
-                format_verb: String::new(),
-                format_prec: 0,
-                format_float: false,
-                origin: 1,
-                seed: SystemTime::now()
-                    .duration_since(UNIX_EPOCH)
-                    .unwrap()
-                    .as_secs(),
-                max_bits: 1_000_000,
-                max_digits: 10_000,
-                max_stack: 100_000,
-                float_prec: 256,
-                real_time: Duration::default(),
-                user_time: Duration::default(),
-                sys_time: Duration::default(),
-                input_base: 0,
-                output_base: 0,
-            }
-        }
-    }
-}
+pub mod config;
 pub mod exec;
 pub mod parse;
 pub mod run;
