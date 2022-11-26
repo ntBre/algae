e9aa5546aa5257a6f160b9125d31c559ca952f07
Brent Westbrook <brentrwestbrook@gmail.com>
2022-11-26 16:39:08 -0500

    allow reading commands from a file

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,15 @@ fn main() {
     // flags
     let conf = Config::default();
     let context = RwLock::new(Context::new(&conf));
-    let scanner = Scanner::new(&context, "<stdin>", std::io::stdin());
-    let mut parser = Parser::new("<stdin>", scanner, &context);
-    while parser.run(&conf, &context, true).is_err() {}
+    let mut args = std::env::args();
+    if let Some(infile) = args.nth(1) {
+        let f = std::fs::File::open(infile).expect("failed to open file");
+        let scanner = Scanner::new(&context, "file", Box::new(f));
+        let mut parser = Parser::new("<stdin>", scanner, &context);
+        while parser.run(&conf, &context, true).is_err() {}
+    } else {
+        let scanner = Scanner::new(&context, "<stdin>", std::io::stdin());
+        let mut parser = Parser::new("<stdin>", scanner, &context);
+        while parser.run(&conf, &context, true).is_err() {}
+    }
 }
